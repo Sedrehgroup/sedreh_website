59098d75f20458fc06db8e1fe3bdc5556befd606
Alireza <mdinvarzade@gmail.com>
2022-12-01 18:16:16 +0330

    modify domain

--- frontend/components/System/apiUrl.js
+++ frontend/components/System/apiUrl.js
@@ -1,5 +1,2 @@
-//export const API = "https://sedrehai.ir";
-//export const GEO_API = "http://195.248.242.106:8080";
-
-export const API = "https://localhost";
-export const GEO_API = "http://localhost:8080";
+export const API = "https://195.248.242.106";
+export const GEO_API = "http://195.248.242.106:8080";
