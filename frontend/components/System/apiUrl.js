d3fc4e6746e5b4e75ccff51f0c5dfcf28f3465b1
Alireza <mdinvarzade@gmail.com>
2022-12-01 17:45:33 +0330

    change url site

--- frontend/components/System/apiUrl.js
+++ frontend/components/System/apiUrl.js
@@ -1,2 +1,5 @@
-export const API = "https://sedrehai.ir";
-export const GEO_API = "http://195.248.242.106:8080";
+//export const API = "https://sedrehai.ir";
+//export const GEO_API = "http://195.248.242.106:8080";
+
+export const API = "https://localhost";
+export const GEO_API = "http://localhost:8080";
