44681a6e3b7aa9912a715bf00f97dc18563ec583
Alireza <mdinvarzade@gmail.com>
2022-12-07 11:31:42 +0330

    add log files celery and celery worker

--- api_django/cel_period_run.py
+++ api_django/cel_period_run.py
@@ -1,13 +1,13 @@
-from django_celery_beat.models import PeriodicTask , CrontabSchedule , IntervalSchedule
+from django_celery_beat.models import PeriodicTask, CrontabSchedule, IntervalSchedule
 
 
 # inter , _ = IntervalSchedule.objects.update_or_create(every=10, period=IntervalSchedule.SECONDS)
-at_3_am , _ = CrontabSchedule.objects.get_or_create(minute="0", hour="3", day_of_week="*", day_of_month="*", month_of_year="*",)
+at_3_am, _ = CrontabSchedule.objects.get_or_create(minute="0", hour="3", day_of_week="*", day_of_month="*", month_of_year="*",)
 
 PeriodicTask.objects.update_or_create(
     name='delete_images',
-    defaults = {
-        'crontab' : at_3_am,
-        'task' : 'apis.tasks.delete_images'
+    defaults={
+        'crontab': at_3_am,
+        'task': 'apis.tasks.delete_images'
     }
 )
--- api_django/config/celery_conf.py
+++ api_django/config/celery_conf.py
@@ -2,7 +2,6 @@ from celery import Celery
 from datetime import timedelta
 import os
 from celery.schedules import crontab
-# import apis.tasks
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
@@ -19,10 +18,3 @@ celery_app.conf.result_expires = timedelta(days=1)
 celery_app.conf.task_always_eager = False
 celery_app.conf.worker_prefetch_multiplier = 1
 celery_app.conf.CELERY_BEAT_SCHEDULER = 'django-celery-beat.schedulers.DatabaseScheduler'
-
-# celery_app.conf.CELERY_BEAT_SCHEDULE = {
-#     "delete_task": {
-#         "task": "apis.tasks.delete_images",
-#         "schedule": crontab(minute='*/1'),
-#     },
-# }
